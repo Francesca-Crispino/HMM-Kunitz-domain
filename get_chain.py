84d0f304469807807ef0a54d4a0b72e54f4825cb
Francesca-Crispino <francesca.crispino@icloud.com>
2026-05-17 22:17:54 +0200

    Delete get_chain.py

--- get_chain.py
+++
@@ -1,9 +0,0 @@
-#!/urs/bin/LAB1 python
-import sys 
-pdbfile=sys.argv[1]
-chain=sys.argv[2]
-
-fh=open(pdbfile)
-
-for line in fh:
-    if line.startswith('ATOM') and line[21]==chain: print (line.rstrip()) 
