af2e12f5537c0dcebe3ba1dbdeb3ee2369a2dd30
Francesca-Crispino <francesca.crispino@icloud.com>
2026-05-17 21:59:38 +0200

    Add files via upload

---
+++ get_chain.py
@@ -0,0 +1,9 @@
+#!/urs/bin/LAB1 python
+import sys 
+pdbfile=sys.argv[1]
+chain=sys.argv[2]
+
+fh=open(pdbfile)
+
+for line in fh:
+    if line.startswith('ATOM') and line[21]==chain: print (line.rstrip()) 
